e6c439a49cc325b9717514c68509df363b73942f
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2023-11-08 12:28:24 +0100

    first commit

---
+++ constants/styles.js
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary50: "#E8EAF6",
+  primary100: "#C5CAE9",
+  primary200: "#9FA8DA",
+  primary500: "#3F51B5",
+  primary700: "#303F9F",
+  primary800: "#283593",
+  accent500: "#3D5AFE",
+  gray700: "#616161",
+  error100: '#FFCCBC',
+  error500: '#FF5722',
+};
